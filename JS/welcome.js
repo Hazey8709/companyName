546b0fccc9b3c11f8698f23d2f025b68069abe9a
Hazey8709 <jfortin802@gmail.com>
2023-03-31 17:24:12 -0400

    ud

--- JS/welcome.js
+++ JS/welcome.js
@@ -3,44 +3,23 @@ let count = localStorage.getItem("visitorCount");
 
 //* If the count doesn't exist in local storage, set it to 0
 if (!count) {
-    count = 0;
+    count = 82;
 }
-
-//* Increment the count by 1
-count++;
-
-//* Save the new count to local storage
-localStorage.setItem("visitorCount", count);
 
 //* Update the visitor count in the HTML
 document.getElementById("visits").textContent = count;
 
+//* Add an event listener to the button to increment the count when clicked
+document
+    .getElementById("increment-visit-count")
+    .addEventListener("click", function () {
+        //* Increment the count by 1
+        count++;
 
+        //* Save the new count to local storage
+        localStorage.setItem("visitorCount", count);
 
+        //* Update the visitor count in the HTML
+        document.getElementById("visitor-count").textContent = count;
+    });
 
-
-// function websiteVisits(response) {
-//     document.querySelector("#visits").textContent = response.value;
-// }
-
-
-// //! Create KEY
-// https://api.countapi.xyz/create?namespace={name of website}&enable_reset=1
-
-//! Reset value Of Counter
-// https://api.countapi.xyz/set/spiltwood/ce90a8cb-af83-4fdc-9f33-a037bee47b51?value=0
-
-// //! HTML script TAG
-// <script
-//     async
-//     src="https://api.countapi.xyz/hit/spiltwood/ce90a8cb-af83-4fdc-9f33-a037bee47b51?callback=websiteVisits"
-// ></script>
-
-// //!        URL INFO
-// {
-
-//     "namespace": "spiltwood",npm
-//     "key": "ce90a8cb-af83-4fdc-9f33-a037bee47b51",
-//     "value": 0
-
-// }
