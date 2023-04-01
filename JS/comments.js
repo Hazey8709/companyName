2c95d8b350930dad651f8b69e2d4c5173e0b5f67
Hazey8709 <jfortin802@gmail.com>
2023-04-01 14:23:00 -0400

    ud

--- JS/comments.js
+++ JS/comments.js
@@ -1,106 +1,101 @@
 //!        Comments  Page   JS
 
-window.onbeforeunload = () => {
-    for (const form of document.getElementsByTagName("form")) {
-        form.reset();
-    }
-};
-
 const commentsList = [
     {
-        Id: 1,
-        Name: "Ben",
-        Comment: "CompanyName is outstanding.",
+        id: 1,
+        name: "Ben",
+        comment: "CompanyName is outstanding.",
     },
     {
-        Id: 2,
-        Name: "Jay",
-        Comment: "CompanyName was fast clean & cheaper.",
+        id: 2,
+        name: "Jay",
+        comment: "CompanyName was fast clean & cheaper.",
     },
     {
-        Id: 3,
-        Name: "Glen",
-        Comment: "CompanyName was great!",
+        id: 3,
+        name: "Glen",
+        comment: "CompanyName was great!",
     },
     {
-        Id: 4,
-        Name: "Blue Davis",
-        Comment: "I was in a jam, CompanyName helped me through it.",
+        id: 4,
+        name: "Blue Davis",
+        comment: "I was in a jam, CompanyName helped me through it.",
     },
 ];
 
-buildList(commentsList);
+//* Get the form and the comments container
+const form = document.querySelector("form");
+const commentsContainer = document.querySelector("#commentsContainer");
 
-function buildList(data) {
-    const list = document.getElementById("commentsContainer");
+//* Load existing comments from local storage, or use an empty array if there are no existing comments
+const comments = JSON.parse(localStorage.getItem("comments")) || commentsList;
 
-    for (var i = 0; i < data.length; i++) {
-        let commentsCont = document.createElement("div");
-        commentsCont.style.textAlign = "center";
-        commentsCont.className = "divCont";
+//* Display existing comments on page load
+displayComments();
 
-        let commentsName = document.createElement("p");
-        commentsName.innerText = `${data[i].Name}`;
-        commentsCont.appendChild(commentsName);
-        commentsName.className = "commentsNameTitle";
+//* Listen for form submission
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // prevent the form from submitting normally
 
-        let comments = document.createElement("p");
-        comments.innerText = `${data[i].Comment}`;
-        commentsCont.appendChild(comments);
-        comments.className = "commentsDetails";
+    //* Get the values of the name and comment fields
+    const name = form.elements.name.value;
+    const comment = form.elements.comment.value;
 
-        list.appendChild(commentsCont);
-    }
-}
+    //* Add the new comment to the array
+    comments.push({ name, comment });
 
-let submitBtn = document.getElementById("submit-btn");
+    //* Save the updated comments array to local storage
+    localStorage.setItem("comments", JSON.stringify(comments));
 
-submitBtn.addEventListener("click", function () {
-    let userName = document.getElementById("name").value;
-    let userComment = document.getElementById("comment").value;
+    //* Display the new comment on the page
+    displayComments();
 
-    const newCom = document.getElementById("commentsContainer");
-
-    let commentsCont = document.createElement("div");
-    commentsCont.style.textAlign = "center";
-    commentsCont.className = "divCont";
-
-    let commentsName = document.createElement("p");
-    commentsName.innerText = `${userName}`;
-    commentsCont.appendChild(commentsName);
-    commentsName.className = "commentsNameTitle";
-
-    let comments = document.createElement("p");
-    comments.innerText = userComment;
-    commentsCont.appendChild(comments);
-    comments.className = "commentsDetails";
-
-    newCom.appendChild(commentsCont);
-
-    console.log(userName);
-    console.log(userComment);
+    //* Reset the form
+    form.reset();
 });
 
-// let commentForm = document.form;
+function displayComments() {
+    //* Clear the comments container
+    commentsContainer.innerHTML = "";
 
-// const addComment = (e) => {
-//     e.preventDefault();
+    //* Loop through the comments and create a new div for each one
+    comments.forEach((comment) => {
+        const div = document.createElement("div");
+        div.textContent = `${comment.name}: ${comment.comment}`;
+        commentsContainer.appendChild(div);
+    });
+}
 
-//     document.getElementsByClassName("form");
-//     let comNameTitle = commentForm.Name.value;
-//     let comDetails = commentForm.Comment.value;
-
-//     let data = {
-//         name: comNameTitle,
-//         Comment: comDetails,
-//     };
+//! Form Spree Reset Form
+// window.onbeforeunload = () => {
+//     for (const form of document.getElementsByTagName("form")) {
+//         form.reset();
+//     }
 // };
 
-// document.getElementById("submit-btn").addEventListener("click");
-// {
-//     let userName = document.getElementById("name").value;
-//     let userComment = document.getElementById("comment").value;
+//! HTML FORM Tag for above
+// action="https://formspree.io/f/xayzypaj"
+//                 method="POST"
 
-//     console.log(userName);
-//     console.log(userComment);
-// }
+// const commentsList = [
+//     {
+//         Id: 1,
+//         Name: "Ben",
+//         Comment: "CompanyName is outstanding.",
+//     },
+//     {
+//         Id: 2,
+//         Name: "Jay",
+//         Comment: "CompanyName was fast clean & cheaper.",
+//     },
+//     {
+//         Id: 3,
+//         Name: "Glen",
+//         Comment: "CompanyName was great!",
+//     },
+//     {
+//         Id: 4,
+//         Name: "Blue Davis",
+//         Comment: "I was in a jam, CompanyName helped me through it.",
+//     },
+// ];
